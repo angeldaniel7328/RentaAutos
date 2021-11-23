27570674638b5b2cb03f053fec2a22ee31480af7
Angel Daniel Lopez Vazquez <50413286+angeldaniel7328@users.noreply.github.com>
2021-11-22 19:21:53 -0600

    Agregar archivos de proyecto.

---
+++ DataAccess/Conexion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+namespace DataAccess
+{
+    public class Conexion
+    {
+        public string CadenaConexion { get; private set; }
+
+        public Conexion() => Configurar();
+
+        private void Configurar()
+        {
+            try
+            {
+                CadenaConexion = ConfigurationManager.AppSettings.Get("CADENA_CONEXION");
+                //DbProviderFactory factory = DbProviderFactories.GetFactory();
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("Error al configurar la cadena de conexión");
+            }
+        }
+    }
+}
---
+++ Entities/VOAutomovil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Entities
+{
+    public class VOAutomovil
+    {
+        public int IdAutomovil { get; set; }
+        public string Matricula { get; set; }
+        public string Modelo { get; set; }
+        public double? Precio { get; set; }
+        public int IdOwner { get; set; }
+        public bool? Disponibilidad { get; set; }
+        public string UrlFoto { get; set; }
+
+    }
+}
