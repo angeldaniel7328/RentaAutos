90d68bb4009a89756b0782a84119a07144b21b29
Angel Daniel Lopez Vazquez <50413286+angeldaniel7328@users.noreply.github.com>
2021-11-28 20:30:28 -0600

    Corrección de entidades

---
+++ Entities/VODevolucion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class VODevolucion
+    {
+        public int IdDevolucion { get; set; }
+        public DateTime? FechaHora { get; set; }
+        public double? CuotaTotal { get; set; }
+        public int? IdRenta { get; set; }
+    }
+}
--- Entities/VORenta.cs
+++ Entities/VORenta.cs
@@ -5,9 +5,8 @@ namespace Entities
     public class VORenta
     {
         public int IdRenta { get; set; }
-        public DateTime? FechaHoraRentad { get; set; }
-        public DateTime? FechaHoraDevolucion { get; set; }
-        public int? Estado { get; set; }
+        public DateTime? FechaHora { get; set; }
+        public string Estado { get; set; }
         public int? IdAutomovil { get; set; }
         public int? IdCliente { get; set; }
 
