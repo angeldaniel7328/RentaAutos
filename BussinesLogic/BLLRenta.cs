003e8409aa894afe7ae28447d54e935613a5c764
Angel Daniel Lopez Vazquez <50413286+angeldaniel7328@users.noreply.github.com>
2021-11-30 18:54:28 -0600

    Agregación de la clase BLLRenta

---
+++ BussinesLogic/BLLRenta.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+using DataAccess;
+
+namespace BussinesLogic
+{
+    public static class BLLRenta
+    {
+        public static bool InsertarRenta(VORenta renta)
+        {
+            try
+            {
+                return DALRenta.InsertarRenta(renta);
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Ocurrio un error " + ex.Message);
+            }
+        }
+
+        public static VORenta ConsultarRentaPorId(string idRenta)
+        {
+            VORenta renta;
+            try
+            {
+                renta = DALRenta.ConsultarRentaPorId(int.Parse(idRenta));
+                if (renta.Equals(null))
+                    throw new ArgumentException("El id buscado no existe en la base de datos");
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Ocurrio un error " + ex.Message);
+            }
+            return renta;
+        }
+
+        public static List<VORenta> ConsultarRentas()
+        {
+            List<VORenta> rentas = new List<VORenta>();
+            try
+            {
+                rentas = DALRenta.ConsultarRentas();
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Ocurrio un error " + ex.Message);
+            }
+            return rentas;
+        }
+
+        public static List<VORentaExtendida> ConsultarRentasExtendidas()
+        {
+            List<VORentaExtendida> rentas = new List<VORentaExtendida>();
+            try
+            {
+                rentas = DALRenta.ConsultarRentasExtendidas();
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Ocurrio un error " + ex.Message);
+            }
+            return rentas;
+        }
+
+        public static VORentaExtendida ConsultarRentaExtendidaPorId(string idRenta)
+        {
+            VORentaExtendida renta;
+            try
+            {
+                renta = DALRenta.ConsultarRentaExtendidaPorId(int.Parse(idRenta));
+                if (renta.Equals(null))
+                    throw new ArgumentException("El id buscado no existe en la base de datos");
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Ocurrio un error " + ex.Message);
+            }
+            return renta;
+        }
+
+        public static bool DevolverAutomovil(string idRenta)
+        {
+            try
+            {
+                return DALRenta.DevolverAutomovil(int.Parse(idRenta));
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Ocurrio un error " + ex.Message);
+            }
+        }
+    }
+}
