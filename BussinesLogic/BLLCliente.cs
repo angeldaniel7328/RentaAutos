3159e214af1a4875f75244900a4a2b851b6dcfae
Angel Daniel Lopez Vazquez <50413286+angeldaniel7328@users.noreply.github.com>
2021-11-30 18:39:42 -0600

    Agregación de la clase BLLCliente

---
+++ BussinesLogic/BLLCliente.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+using DataAccess;
+
+namespace BussinesLogic
+{
+    public static class BLLCliente
+    {
+        public static bool InsertarCliente(VOCliente cliente)
+        {
+            try
+            {
+                return DALCliente.InsertarCliente(cliente);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Ocurrio un error " + e.Message);
+            }
+        }
+
+        public static bool Actualizarcliente(VOCliente cliente)
+        {
+            try
+            {
+                return DALCliente.ActualizarCliente(cliente);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Ocurrio un error " + e.Message);
+            }
+        }
+
+        public static bool Eliminarcliente(string idCliente)
+        {
+            try
+            {
+                return DALCliente.EliminarCliente(int.Parse(idCliente));
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Ocurrio un error " + e.Message);
+            }
+        }
+
+        public static VOCliente ConsultarclientePorId(string idCliente)
+        {
+            VOCliente cliente;
+            try
+            {
+                cliente = DALCliente.ConsultarClientePorId(int.Parse(idCliente));
+                if (cliente.Equals(null))
+                    throw new ArgumentException("El id buscado no existe en la base de datos");
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Ocurrio un error " + e.Message);
+            }
+            return cliente;
+        }
+
+        public static List<VOCliente> Consultarclientes()
+        {
+            List<VOCliente> clientes;
+            try
+            {
+                clientes = DALCliente.ConsultarClientes();
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Ocurrio un error " + e.Message);
+            }
+            return clientes;
+        }
+
+    }
+}
